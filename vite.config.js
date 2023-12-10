daf461ee6c0b2e907ac7d960aab1269abb759a39
projecteleven-11 <153259863+projecteleven-11@users.noreply.github.com>
2023-12-10 13:53:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/advance-home-decor/",
+  // base:"/advance-home-decor/",
   plugins: [react()],
   server: {
     open: true,
